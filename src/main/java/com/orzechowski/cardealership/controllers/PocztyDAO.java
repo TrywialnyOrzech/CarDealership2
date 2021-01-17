6354658fea67b110f6761dd4b872657e7a560fb0
Orzechowski Natan (STUD) <01143773@pw.edu.pl>
2021-01-17 16:16:41 +0100

    Configure poczty repository

---
+++ src/main/java/com/orzechowski/cardealership/controllers/PocztyDAO.java
@@ -0,0 +1,47 @@
+package com.orzechowski.cardealership.controllers;
+
+import com.orzechowski.cardealership.models.Poczty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class PocztyDAO {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public PocztyDAO(JdbcTemplate jdbcTemplate) {
+        super();
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    // List with data from table
+    public List<Poczty> list(){
+        String sql = "SELECT * FROM Poczty";
+        List<Poczty> listPoczty = jdbcTemplate.query(sql, BeanPropertyRowMapper.newInstance(Poczty.class));
+        return listPoczty;
+    }
+
+    // Insert row
+    public void save(Poczty poczty){
+
+    }
+
+    // Read row
+    public Poczty get(int id){
+        return null;
+    }
+
+    // Update row
+    public void update(Poczty poczty){
+
+    }
+
+    // Delete row
+    public void delete(int id){
+
+    }
+}
