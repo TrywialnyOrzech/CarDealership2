e92e292662d165530da01f1309e8fde7d538e081
Orzechowski Natan (STUD) <01143773@pw.edu.pl>
2021-01-17 15:57:42 +0100

    Add Poczty and PocztyDAO classes

---
+++ src/main/java/com/orzechowski/cardealership/models/Poczty.java
@@ -0,0 +1,41 @@
+package com.orzechowski.cardealership.models;
+
+public class Poczty {
+    private int nr_poczty;
+    private String kod_poczty;
+    private String Poczta;
+
+    public Poczty(){
+
+    }
+
+    public Poczty(int nr_poczty, String kod_poczty, String poczta) {
+        this.nr_poczty = nr_poczty;
+        this.kod_poczty = kod_poczty;
+        Poczta = poczta;
+    }
+
+    public int getNr_poczty() {
+        return nr_poczty;
+    }
+
+    public void setNr_poczty(int nr_poczty) {
+        this.nr_poczty = nr_poczty;
+    }
+
+    public String getKod_poczty() {
+        return kod_poczty;
+    }
+
+    public void setKod_poczty(String kod_poczty) {
+        this.kod_poczty = kod_poczty;
+    }
+
+    public String getPoczta() {
+        return Poczta;
+    }
+
+    public void setPoczta(String poczta) {
+        Poczta = poczta;
+    }
+}
---
+++ src/main/java/com/orzechowski/cardealership/models/PocztyDAO.java
@@ -0,0 +1,14 @@
+package com.orzechowski.cardealership.models;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class PocztyDAO {
+    private JdbcTemplate jdbcTemplate;
+
+    // List with data from table
+    public List<Poczty> list(){
+        return null;
+    }
+}
