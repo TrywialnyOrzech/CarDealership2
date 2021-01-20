f9c89d1c52ae934258e7b785092ae91b567ca7ac
Orzechowski Natan (STUD) <01143773@pw.edu.pl>
2021-01-20 12:02:48 +0100

    Add test index page

--- src/main/java/com/orzechowski/cardealership/CarDealershipApplication.java
+++ src/main/java/com/orzechowski/cardealership/CarDealershipApplication.java
@@ -1,9 +1,14 @@
 package com.orzechowski.cardealership;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class CarDealershipApplication {
     public static void main(String[] args){
         SpringApplication.run(CarDealershipApplication.class, args);
     }
 }
+
+
+
