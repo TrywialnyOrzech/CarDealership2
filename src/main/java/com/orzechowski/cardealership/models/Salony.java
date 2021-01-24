ed8a3aa0c3cd1e3be680772e194a7b83cf13aeda
Orzechowski Natan (STUD) <01143773@pw.edu.pl>
2021-01-24 16:06:11 +0100

    Implement Salony class.

--- src/main/java/com/orzechowski/cardealership/models/Salony.java
+++ src/main/java/com/orzechowski/cardealership/models/Salony.java
@@ -1,4 +1,50 @@
 package com.orzechowski.cardealership.models;
 
 public class Salony {
+    private int nr_salonu;
+    private String nazwa;
+    private char status;
+    private int nr_biura;
+
+    public Salony() {
+
+    }
+
+    public Salony(String nazwa, char status, int nr_biura) {
+        this.nazwa = nazwa;
+        this.status = status;
+        this.nr_biura = nr_biura;
+    }
+
+    public int getNr_salonu() {
+        return nr_salonu;
+    }
+
+    public void setNr_salonu(int nr_salonu) {
+        this.nr_salonu = nr_salonu;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public char getStatus() {
+        return status;
+    }
+
+    public void setStatus(char status) {
+        this.status = status;
+    }
+
+    public int getNr_biura() {
+        return nr_biura;
+    }
+
+    public void setNr_biura(int nr_biura) {
+        this.nr_biura = nr_biura;
+    }
 }
